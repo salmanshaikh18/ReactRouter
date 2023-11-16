b4afaf5c92ff6f27b02e0a346903111160999dbc
Salman <salmanshaikh18786@gmail.com>
2023-11-16 20:58:20 +0530

    about component and routing in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider }
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
