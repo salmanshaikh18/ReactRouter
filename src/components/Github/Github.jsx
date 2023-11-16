02db7bb56bd898dc46f485079f9ca0ef6e52f595
Salman <salmanshaikh18786@gmail.com>
2023-11-16 21:38:10 +0530

    get image and followers from github

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Github = () => {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/salmanshaikh18')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setData(data)
+        })
+    }, [])
+
   return (
-    <div className='text-center m-5 bg-gray-500 text-white p-4 text-3xl'>Github Follwers: </div>
+    <div className='text-center m-5 bg-gray-500 text-white p-4 text-3xl'>
+        Github Follwers: {data.followers} 
+        <img className='' src={data.avatar_url} alt="GitHub Profile Picture" />
+    </div>
   )
 }
 
