16fb3daffd10acf13c0d23d542bfcbb0e82cdc4b
Salman <salmanshaikh18786@gmail.com>
2023-11-16 21:29:10 +0530

    github component

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Github = () => {
+  return (
+    <div className='text-center m-5 bg-gray-500 text-white p-4 text-3xl'>Github Follwers: </div>
+  )
+}
+
+export default Github
