d6b4082d673f275e4a628f8bbf5072b5900373d4
Salman <salmanshaikh18786@gmail.com>
2023-11-16 21:54:10 +0530

    useLoaderData

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const Github = () => {
 
-    const [data, setData] = useState([])
+    const data = useLoaderData()
 
-    useEffect(() => {
-        fetch('https://api.github.com/users/salmanshaikh18')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            setData(data)
-        })
-    }, [])
+    // const [data, setData] = useState([])
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/salmanshaikh18')
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // }, [])
 
   return (
     <div className='text-center m-5 bg-gray-500 text-white p-4 text-3xl'>
@@ -24,3 +27,8 @@ const Github = () => {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/salmanshaikh18')
+    return response.json()
+}
